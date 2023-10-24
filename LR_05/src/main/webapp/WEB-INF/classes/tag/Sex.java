80e6c069fbef76889a372e2f88eb280715af4706
Skilsar <skilsar@gmail.com>
2023-10-24 18:47:50 +0300

    Add LR_05

---
+++ LR_05/src/main/webapp/WEB-INF/classes/tag/Sex.java
@@ -0,0 +1,24 @@
+package tag;
+
+import jakarta.servlet.jsp.tagext.TagSupport;
+
+public class Sex extends TagSupport {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int doStartTag() {
+        try {
+            pageContext.getOut().write(String.format("<input type='radio' name='%s' value='male'>Male</input>", name));
+            pageContext.getOut()
+                    .write(String.format("<input type='radio' name='%s' value='female'>Female</input>", name));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return SKIP_BODY;
+    }
+}
---
+++ LR_05/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>LR_05</title>
+  </head>
+  <body>
+    <p><a href="Ttt.jsp">Go to Ttt.jsp</a></p>
+  </body>
+</html>
